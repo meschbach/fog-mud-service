9cd8751c4901b13a326200d929fe4e10a8cc6bf8
Mark Eschbach <meschbach@gmail.com>
2019-04-25 15:12:50 -0700

    fs-storage: Moves under node group

--- fs-node.js
+++ fs-node.js
@@ -1,35 +1,6 @@
-const assert = require('assert');
 
-const storageAPI = require("./node/http-v1");
-const {Context} = require("junk-bucket/context");
-const {express_server, JailedVFS, LocalFileSystem} = require("./junk");
-
-function http_v1(logger, system, config) {
-	const rootContext = new Context("fs-node", logger);
-
-	assert(config.storage, "Storage not defined.");
-	const localFS = new LocalFileSystem();
-	const jailRoot = config.storage;
-	const jailedFS = new JailedVFS(jailRoot, localFS);
-	const storageAPI_v1 = storageAPI.http_v1(rootContext, jailedFS);
-
-
-	const address = express_server(rootContext, storageAPI_v1, config.port, "127.0.0.1");
-	const oldAddress = address.then((a) => {
-		const parts = a.split(":")
-		return {
-			address: parts[0],
-			port: parseInt(parts[1])
-		}
-	});
-
-	return {
-		address: oldAddress,
-		end: async function(){
-			await rootContext.cleanup();
-		}
-	};
-}
+const {localFSStorage} = require("./node/fs-storage");
+const http_v1 = localFSStorage;
 
 const { CoordinatorHTTPClient } = require("./metadata/coordinator");
 
